19ed689a5c7e22fba3bd97637bc28ce48eb37904
fabriciosilvaJr <fabriciosilva187@gmail.com>
2024-05-01 19:11:13 -0300

    layout do formulario nova tarefa

--- src/views/FormTask.vue
+++ src/views/FormTask.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>
-    <h1>Form</h1>
+  <div class="container mt-2">
+    <b-form>
+      <b-form-group label="Titulo" label-for="title">
+        <b-form-input
+          id="title"
+          v-model="form.title"
+          type="text"
+          placeholder="Ex Estudar php"
+          required
+          autocomplete="off"
+        >
+        </b-form-input>
+      </b-form-group>
+      <b-form-group label="Descrição" label-for="description">
+        <b-form-textarea
+          id="description"
+          v-model="form.description"
+          type="text"
+          placeholder="Orientação a objeto e suas propriedades"
+          required
+          autocomplete="off"
+        >
+        </b-form-textarea>
+      </b-form-group>
+      <b-button type="submit" variant="outline-primary">Salvar</b-button>
+    </b-form>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: "FormTask",
+  data(){
+    return{
+      form:{
+        title: "",
+        description: ""
+      }
+    }
+  }
+}
+</script>
