d66a03667e6fcd830bf8ce9768ec44023ca2707a
fabriciosilvaJr <fabriciosilva187@gmail.com>
2024-05-02 15:45:38 -0300

    renomeando item do menu para adicionar tarefa

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item to="/">Tarefas</b-nav-item>
-          <b-nav-item to="/form">Nova</b-nav-item>
+          <b-nav-item to="/form">Nova Tarefa</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
 
