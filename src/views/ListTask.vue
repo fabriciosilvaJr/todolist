01c866d47cfd49af3af3b4461b741b6641d72616
fabriciosilvaJr <fabriciosilva187@gmail.com>
2024-05-01 22:40:23 -0300

    metodo para deletar uma tarefa

--- src/views/ListTask.vue
+++ src/views/ListTask.vue
@@ -4,9 +4,20 @@
       <b-card :title="task.title" class="mb-2">
         <b-card-text>{{task.description}}</b-card-text>
         <b-button variant="outline-secondary" class="mr-2" @click="edit(index)">Editar</b-button>
-        <b-button variant="outline-danger" class="mr-2">Excluir</b-button>
+        <b-button variant="outline-danger" class="mr-2" @click="remove(task,index)">Excluir</b-button>
       </b-card>
     </div>
+  
+    <b-modal ref="modalRemove" hide-footer title="Exclusão de tarefa">
+      <div class="d-block text-center">
+        Tem certeza que deseja excluir essa  {{taskSelected.title}}?
+      </div>
+      <div class="mt-3 d-flex justify-content-end">
+        <b-button variant="outline-secondary" class="mr-2" @click="hideModal">Cancelar</b-button>
+        <b-button variant="outline-danger" class="mr-2" @click="confirmRemoveTask">Excluir</b-button>
+
+      </div>
+    </b-modal>
   </div>
 </template>
 
@@ -16,6 +27,8 @@ export default {
   data() {
     return {
       tasks: [],
+      taskSelected: []
+
     };
   },
 
@@ -28,6 +41,21 @@ export default {
   methods:{
     edit(index){
       this.$router.push({name:"form",params: {index}});
+    },
+    remove(task, index){
+      this.taskSelected = task;
+       this.taskSelected.index = index;
+       this.$refs.modalRemove.show();
+
+    },
+    hideModal(){
+       this.$refs.modalRemove.hide();
+
+    },
+    confirmRemoveTask(){
+      this.tasks.splice(this.taskSelected.index,1);
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      this.hideModal();
     }
   }
 };
